556fc00b38b80cb95c8247371b5338bf211fccc1
amalboukili <boukili.amal@gmail.com>
2020-02-25 14:24:51 +0100

    BackEnd creation des entities et des repositories

---
+++ cinema/src/main/java/org/sid/cinema/repositories/PlaceRepository.java
@@ -0,0 +1,10 @@
+package org.sid.cinema.repositories;
+
+import org.sid.cinema.entities.Place;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface PlaceRepository extends JpaRepository<Place, Long> {
+
+}
---
+++ cinema/src/main/java/org/sid/cinema/repositories/VilleRepository.java
@@ -0,0 +1,11 @@
+package org.sid.cinema.repositories;
+
+import org.sid.cinema.entities.Ville;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * VilleRepository
+ */
+public interface VilleRepository extends JpaRepository<Ville, Long> {
+
+}
